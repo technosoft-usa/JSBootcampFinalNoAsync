79ca2ed7f8ce3d19ed8c9636a9e25075ee65df15
Macarena Arroyo Gil <macarena@blinkhealth.com>
2023-03-01 20:48:51 -0500

    final tets

--- tests/04_RemoveVowels/removeVowels.js
+++ tests/04_RemoveVowels/removeVowels.js
@@ -1,5 +1,13 @@
-function removeVowels() {
+function removeVowels(str) {
+    const vowels = 'AEIOUaeiou';
+    let finalString = '';
 
+    for (let i = 0; i < str.length; i++) {
+        if (!vowels.includes(str[i])) {
+            finalString += str[i];
+        }
+    }
+    return finalString;
 }
 
 module.exports = { removeVowels };
